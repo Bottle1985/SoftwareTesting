178a34775564a15e525d1ff58480ea77c1dbb0c0
DucXCode <hduc1812.nhduc12b4@gmail.com>
2022-11-14 22:57:17 +0700

    Update Check Ve TBD

--- src/DomainTest.java
+++ src/DomainTest.java
@@ -1,38 +1,39 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DomainTest {
-    private static WebDriver webDriver;
-    public static void setUp(){
-        System.setProperty("webdriver.chrome.driver","lib/chromedriver.exe");
-        webDriver = new ChromeDriver();
-    }
-    public static boolean validateTitle(String expectedTitle){
+	private static WebDriver webDriver;
+
+	public static void setUp() {
+		System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+		webDriver = new ChromeDriver();
+	}
+
+	public static boolean validateTitle(String expectedTitle) {
 		String actualTitle = webDriver.getTitle();
-		if(actualTitle.contentEquals(expectedTitle)){
+		if (actualTitle.contentEquals(expectedTitle)) {
 			return true;
-		}
-		else{
+		} else {
 			return false;
 		}
 	}
-    public static void headingText(){
-        webDriver.get("https://tbd.edu.vn/tra-cuu-ho-so/");
-        webDriver.manage().window().maximize();
-        	String expectedHeading = "Tra cứu hồ sơ - Đại học Thái Bình Dương";
-        	
-        	//Storing the text of the heading in a string
-        	String heading = webDriver.getTitle();
-        	if(expectedHeading.equalsIgnoreCase(heading))
-              	System.out.println("The expected heading is same as actual heading --- "+heading);
-        	else
-              	System.out.println("The expected heading doesn't match the actual heading --- "+heading);
-  	}
 
-    public static void name() {
-        System.out.println("Good luck");
-        setUp();
-        headingText();
-    }
+	public static void headingText() {
+		webDriver.get("https://tbd.edu.vn/tra-cuu-ho-so/");
+		webDriver.manage().window().maximize();
+		String expectedHeading = "Tra cứu hồ sơ - Đại học Thái Bình Dương";
+
+		// Storing the text of the heading in a string
+		String heading = webDriver.getTitle();
+		if (expectedHeading.equalsIgnoreCase(heading))
+			System.out.println("The expected heading is same as actual heading --- " + heading);
+		else
+			System.out.println("The expected heading doesn't match the actual heading --- " + heading);
+	}
+
+	public static void name() {
+		System.out.println("Good luck");
+		setUp();
+		headingText();
+	}
 }
