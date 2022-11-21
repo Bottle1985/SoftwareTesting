ef04749994b9c6fff8f499b5952c115adec340e1
Minh Pham <phammihtuan0304@gmail.com>
2022-11-21 21:19:17 +0700

    check sinh vien link

--- src/App.java
+++ src/App.java
@@ -100,6 +100,7 @@ public class App {
         //DaotaoDTuan.CheckDaotaoTBD();
         //nghiencuuXHong.nghiencuuTBD();
         TienNghiencuu.TuyensinhTBD();
+        TuanSinhVien.SinhVienTBD();
     }
 
 }
